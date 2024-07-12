fe4a4448ca4fbdc6bf4056662554692ae7fe01c8
Royweru <royezi17@gmail.com>
2024-07-12 11:35:00 +0300

    modified:   app/(marketing)/_components/header.tsx 	modified:   middleware.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   public/logo.png

--- app/(marketing)/_components/header.tsx
+++ app/(marketing)/_components/header.tsx
@@ -1,9 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 export const Header = () => {
   return (
     <header className=" h-20 w-full border-b-2 border-slate-200 px-4">
-      Heeader
+      <div className=" h-full mx-auto lg:max-w-screen-lg flex justify-between items-center">
+        <div className=" pb-7 pl-4 pt-8">
+          <Image src={"/logo.png"} width={40} height={40} alt="" />
+        </div>
+        <ClerkLoading>
+          <Loader className=" w-5 h-5 animate-spin text-muted-foreground" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"}>Login</Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   );
 };
