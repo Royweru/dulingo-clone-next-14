86defadd17ba62be91bcb1d442a7c312509b202d
Royweru <royezi17@gmail.com>
2024-07-14 21:24:55 +0300

    modified:   app/(main)/layout.tsx 	new file:   components/mobile-header.tsx 	new file:   components/mobile-sidebar.tsx 	new file:   components/sidebar.tsx 	new file:   components/ui/sheet.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,16 @@
+import { MobileHeader } from "@/components/mobile-header";
+import { Sidebar } from "@/components/sidebar";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className=" pl-[256px] h-full">
-      <div className="  bg-red-500 h-full">{children}</div>
-    </main>
+    <>
+      <MobileHeader />
+      <Sidebar className=" hidden lg:flex" />
+      <main className=" lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+        <div className="  bg-red-500 h-full">{children}</div>
+      </main>
+    </>
   );
 };
 
