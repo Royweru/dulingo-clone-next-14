65bd9812b663177e6bd46ff7bedc9532eda2bc57
Royweru <royezi17@gmail.com>
2024-07-30 06:28:03 +0300

    modified:   actions/user-progress.ts 	modified:   app/(main)/shop/_components/items.tsx 	modified:   db/schema.ts

--- app/(main)/shop/_components/items.tsx
+++ app/(main)/shop/_components/items.tsx
@@ -1,18 +1,30 @@
 "use client";
+import { refillHearts } from "@/actions/user-progress";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+import React, { useTransition } from "react";
+import { toast } from "sonner";
 
 type ItemsProps = {
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
 };
+export const POINTS_TO_REFILL = 10;
 export const Items = ({
   hearts,
   points,
   hasActiveSubscription,
 }: ItemsProps) => {
+  const [pending, startTransition] = useTransition();
+
+  const onRefillHearts = () => {
+    if (pending || hearts === 5 || points < POINTS_TO_REFILL) return;
+
+    startTransition(() => {
+      refillHearts().catch(() => toast.error("Something went wrong"));
+    });
+  };
   return (
     <ul className=" w-full">
       <div className=" flex items-center w-full p-4 gap-x-4 border-t-2">
@@ -22,13 +34,13 @@ export const Items = ({
             Refill hearts
           </p>
         </div>
-        <Button disabled={hearts === 5}>
+        <Button onClick={onRefillHearts} disabled={hearts === 5}>
           {hearts === 5 ? (
             "Full"
           ) : (
             <div className=" flex items-center">
               <Image src={"/points.png"} alt="" height={20} width={20} />
-              <p>50</p>
+              <p>{POINTS_TO_REFILL}</p>
             </div>
           )}
         </Button>
